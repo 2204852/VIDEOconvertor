146e00e300e41d0cae4cd7baa9d0ec4696aee43d
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-04-03 18:56:00 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -22,12 +22,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = 13216322
 API_HASH = "15e5e632a8a0e52251ac8c3ccbe462c7"
 BOT_TOKEN ="7034502580:AAGf4uNwTeHKF6HpkXSGPa97Y6HfXz6yHBo"
-BOT_UN = "@videoxconvertorbot"
+BOT_UN = "videoxconvertorbot"
 AUTH_USERS = 5642570692
 LOG_CHANNEL = "oekendien"
-LOG_ID = -1002017655900
-FORCESUB = "-1001913863954"
-FORCESUB_UN = "@mehulbots"
+LOG_ID = -1002017655900 #go to file action.py to change 
+FORCESUB = "-1001913863954" #go to file action.py to change 
+FORCESUB_UN = "mehulbots"
 ACCESS_CHANNEL = -1002017655900
 MONGODB_URI = "mongodb+srv://user123456:123456user@cluster0.fysntw5.mongodb.net/"
 
