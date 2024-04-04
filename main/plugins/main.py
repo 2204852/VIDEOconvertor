6fd55caf70edf26277f7ded82809aecb71bd2816
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-04-04 17:30:53 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -81,21 +81,21 @@ async def _encode(event):
                          Button.inline("x265", data="265")],
                         [Button.inline("BACK", data="back")]])
      
-#@Drone.on(events.callbackquery.CallbackQuery(data="compress"))
-#async def _compress(event):
-#    await event.edit("If You want to compress video, You can use our other bot @videocompxbot",
-#                    buttons=[
-#                        [Button.url("Compress video (Move to Bot)", url="https://t.me/videocompxbot")],
-#                        [Button.inline("Back", data="back")]
-#                    ])
-
 @Drone.on(events.callbackquery.CallbackQuery(data="compress"))
 async def _compress(event):
-    await event.edit("**🗜COMPRESS**",
+    await event.edit("If You want to compress video, You can use our other bot @videocompxbot",
                     buttons=[
-                        [Button.inline("HEVC COMPRESS", data="hcomp"),
-                         Button.inline("FAST COMPRESS", data="fcomp")],
-                        [Button.inline("BACK", data="back")]])
+                        [Button.url("Compress video (Move to Bot)", url="https://t.me/videocompxbot")],
+                        [Button.inline("Back", data="back")]
+                    ])
+
+#@Drone.on(events.callbackquery.CallbackQuery(data="compress"))
+#async def _compress(event):
+#    await event.edit("**🗜COMPRESS**",
+#                    buttons=[
+#                        [Button.inline("HEVC COMPRESS", data="hcomp"),
+#                         Button.inline("FAST COMPRESS", data="fcomp")],
+#                        [Button.inline("BACK", data="back")]])
     
     
 @Drone.on(events.callbackquery.CallbackQuery(data="convert"))
